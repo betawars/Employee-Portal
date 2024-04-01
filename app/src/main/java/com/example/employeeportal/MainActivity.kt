02b607559e981b5c0b518b5419ae2b1045873a1c
Shashank Sanjay Betawar <betawars@oregonstate.edu>
2024-04-01 12:59:38 -0700

    Working changes

--- app/src/main/java/com/example/employeeportal/MainActivity.kt
+++ app/src/main/java/com/example/employeeportal/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
@@ -24,6 +25,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var username: TextView
     private lateinit var password: TextView
     private lateinit var saveButton: Button
+    private var ifError: String = "false"
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,66 +38,96 @@ class MainActivity : AppCompatActivity() {
         saveButton = findViewById(R.id.save_button)
 
         val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(baseContext)
-        val editor: SharedPreferences.Editor = sharedPrefs.edit()
         val checkUsername = sharedPrefs.getString(getString(R.string.username),"")
         val checkPassword = sharedPrefs.getString(getString(R.string.password),"")
-        if (checkPassword == "" && checkUsername == ""){
-            username.visibility = View.VISIBLE
-            password.visibility = View.VISIBLE
-            saveButton.visibility = View.VISIBLE
-            webView.visibility = View.INVISIBLE
-            saveButton.setOnClickListener {
-                editor.putString("username",username.text.toString())
-                editor.putString("password",EncryptionHelper.encrypt(password.text.toString()))
-                editor.apply()
-            }
+
+        if (checkPassword == "" && checkUsername == "" && ifError == "false"){
+            getCredVisuals()
         }else{
-
-            username.visibility = View.INVISIBLE
-            password.visibility = View.INVISIBLE
-            saveButton.visibility = View.INVISIBLE
-            webView.visibility = View.VISIBLE
-            webView.setWebViewClient(object : WebViewClient(){
-                override fun onPageFinished(view: WebView, url: String) {
-                    webView.loadUrl("javascript:(function(){" +
-                            "document.getElementsByClassName('btn btn-large btn-primary color-active')[0].click();" +
-                            "})()")
-                    fillFields(checkUsername.toString(),checkPassword.toString())
-                    pressLogin()
-                }
-            })
-
-            webView.loadUrl("https://mytime.oregonstate.edu/")
-            webView.settings.javaScriptEnabled = true
+            getClickVisuals(checkUsername,checkPassword)
         }
 
+    }
+
+    private fun getCredVisuals(){
+        ifError = "false"
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(baseContext)
+        val editor: SharedPreferences.Editor = sharedPrefs.edit()
+        username.visibility = View.VISIBLE
+        password.visibility = View.VISIBLE
+        saveButton.visibility = View.VISIBLE
+        webView.visibility = View.INVISIBLE
+        saveButton.setOnClickListener {
+            editor.putString("username",username.text.toString())
+            editor.putString("password",EncryptionHelper.encrypt(password.text.toString()))
+            editor.apply()
+            val checkUsername = sharedPrefs.getString(getString(R.string.username),"")
+            val checkPassword = sharedPrefs.getString(getString(R.string.password),"")
+            getClickVisuals(checkUsername,checkPassword)
+        }
+    }
+
+    private fun getClickVisuals(checkUsername:String?,checkPassword:String?){
+        username.visibility = View.INVISIBLE
+        password.visibility = View.INVISIBLE
+        saveButton.visibility = View.INVISIBLE
+        webView.visibility = View.VISIBLE
+        webView.setWebViewClient(object : WebViewClient(){
+            override fun onPageFinished(view: WebView, url: String) {
+                webView.loadUrl("javascript:(function(){" +
+                        "document.getElementsByClassName('btn btn-large btn-primary color-active')[0].click();" +
+                        "})()")
+                fillFields(checkUsername.toString(),checkPassword.toString())
+                pressLogin()
+            }
+        })
+
+        webView.loadUrl("https://mytime.oregonstate.edu/")
+        webView.settings.javaScriptEnabled = true
     }
 
     private fun fillFields(username:String,password:String){
         val javascript = """
                     (function() {
                         document.getElementById('username').value = '${username}';
-                        document.getElementById('password').value = '${password}';
+                        document.getElementById('password').value = '${EncryptionHelper.decrypt(password)}';
                     })();
                 """.trimIndent()
         webView.evaluateJavascript(javascript, null)
     }
 //    document.getElementById('password').value = '${EncryptionHelper.decrypt(password)}';
     private fun pressLogin(){
+        var javascript = ""
+        javascript = """
+                   (function() {
+                       var buttons = document.getElementsByName('_eventId_proceed');
+                       if (buttons.length > 0) {
+                           buttons[0].click();
+                       } else {
+                           console.error('Button with name "abc" not found.');
+                       }    
+                       if (Object.values(document.getElementsByClassName('form-element form-error')).at(0).innerText === "Incorrect username or password.") {
+                           return true;
+                       }
+                       
+                   })();
+               """.trimIndent()
 
-        val javascript = """
-                    (function() {
-                        var buttons = document.getElementsByName('_eventId_proceed');
-                        if (buttons.length > 0) {
-                            buttons[0].click();
-                        } else {
-                            console.error('Button with name "abc" not found.');
-                        }
-                    })();
-                """.trimIndent()
-        webView.evaluateJavascript(javascript, null)
+
+        val callback = ValueCallback<String> { value ->
+            if (value!="null" || value != null) {
+                ifError = value
+            } else {
+                Log.d("WebView", "Element not found or value is null or blank")
+            }
+        }
+
+        if (ifError == "false" || ifError == "null" || ifError.isBlank()){
+            webView.evaluateJavascript(javascript, callback)
+
+        }else{
+
+            getCredVisuals()
+        }
     }
-
-
-
 }
